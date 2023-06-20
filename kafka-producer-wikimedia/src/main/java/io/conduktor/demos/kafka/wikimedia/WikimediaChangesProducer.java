34e52d5825d863a82ee4fc3088e152d784d1d620
Bárbara Amaral <barbara.souza.amaral@gmail.com>
2023-06-20 10:58:20 -0300

    Creating the Producer; Creating the Event Handler;

--- kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -1,7 +1,55 @@
 package io.conduktor.demos.kafka.wikimedia;
 
-public class WikimediaChangesProducer {
-    public static void main(String[] args) {
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+    public static void main(String[] args) throws InterruptedException {
+
+        //1. Specify the bootstrap servers
+
+        String bootstrapServers = "127.0.0.1:9092";
+
+        //2. Create producer properties
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //2. Create the Producer
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        // The event handler is going to handle the events coming from the stream
+        // and send them to the producer
+
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange"; //the url from where the stream is coming from
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+
+        eventSource.start();
+
+        // we produce for ten minutes and block the program until then
+
+        TimeUnit.MINUTES.sleep(10);
+
+        // run docker containers
+        // to check consumer on conduktor: access localhost:8080
+        // to check consumer on the command line (ubuntu if on windows):
+        // kafka-console-consumer --bootstrap-server localhost:9092 --topic wikimedia.recentchange
     }
 }
